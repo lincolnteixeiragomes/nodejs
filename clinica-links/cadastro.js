efcd42848f2c5e308df48cd651d0cd24b2bb07b7
Lincoln Teixeira Gomes <lincolnteixeiragomes@gmail.com>
2019-03-06 20:23:14 -0300

    Mudado para pagina inicial, e utilizado o express para escutar a porta 8081

--- clinica-links/cadastro.js
+++
@@ -1,1 +0,0 @@
-console.log("Aqui começa o projeto!!!")
---
+++ clinica-links/index.js
@@ -0,0 +1,11 @@
+const var express = require("express");
+const var app = express();
+
+app.get("/", function(req, res){
+  res.send("Seja muito bem vindo");
+});
+
+
+app.listen(8081, function() {
+  console.log("Servidor rodando no endereço http://192.168.0.16:8081");
+});
