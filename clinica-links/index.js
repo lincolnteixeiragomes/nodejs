6ecd68f76090c98d5b3bd6b29705800ab51a36b8
Lincoln Teixeira Gomes <lincolnteixeiragomes@gmail.com>
2019-03-06 20:38:39 -0300

    Adicionado as rotas sobre e cadastro

--- clinica-links/index.js
+++ clinica-links/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("Seja muito bem vindo");
+  res.send("Seja muito bem vindo!");
 });
-
+app.get("sobre",function(req, res){
+  res.send("Minha pagina sobre!");
+});
+app.get("cadastro", function(req, res){
+  res.send("Essa é minha pagina de cadastro!")
+});
 
 app.listen(8081, function() {
   console.log("Servidor rodando no endereço http://192.168.0.16:8081");
