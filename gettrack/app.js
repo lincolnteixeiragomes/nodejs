5d108c5c43787dc35ad50ba64d9bff9fd8072bdc
Lincoln Teixeira Gomes <lincolnteixeiragomes@gmail.com>
2019-03-26 22:15:27 -0300

    Alinhamento

--- gettrack/app.js
+++ gettrack/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/:data", function(req, res){
-  res.send("Data enviada: " + req.params.data );
+  res.send(" Data enviada: " + req.params.data );
 });
 
 app.listen(6969, function() {
